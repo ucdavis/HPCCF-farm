afc729abdd915533fee278581b53af5a47d4f4ef
Camille Scott <cswel@ucdavis.edu>
2022-09-10 10:04:41 -0700

    Update jmodeltest to load java and link in the phyml binary

--- modules/build/jmodeltest/2.1.10/build.sh
+++ modules/build/jmodeltest/2.1.10/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ./module.sh
+module load jdk
 
 if [ ! -f "jmodeltest-${VERSION}.tar.gz" ]; then
 		curl -L https://github.com/ddarriba/jmodeltest2/archive/refs/tags/v2.1.10r20160303.tar.gz -o jmodeltest-$VERSION.tar.gz
--- modules/build/jmodeltest/2.1.10/install.sh
+++ modules/build/jmodeltest/2.1.10/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 source ./module.sh
+module load phyml
 
 sudo mkdir -p $INSTALL_PREFIX
 sudo mkdir -p `dirname $MODULEFILE_INSTALL_DEST`
 sudo cp $MODULEFILE_SOURCE $MODULEFILE_INSTALL_DEST
 
 sudo cp -r $SRC_DIR/dist/* $INSTALL_PREFIX/
+sudo cp $phyml_HOME/bin/phyml $INSTALL_PREFIX/exe/phyml/PhyML_3.0_linux64
